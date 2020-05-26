e46c5fe665c20907047abf282c509b5ec7506686
dr. Simon Bálint <balladin0@gmail.com>
2020-05-26 14:31:21 +0200

    add shadow picture row basics

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import CoverPicture from "./layout/CoverPicture";
+import LeftContainer from "./layout/LeftContainer";
 
 
 const Home = () => {
   return (
     <React.Fragment>
         <CoverPicture />
+        <div className="container-fluid">
+            <LeftContainer />
+        </div>
       <div>This is the homepage</div>
       <h4>Welcome to our hotel</h4>
     </React.Fragment>
---
+++ src/Components/layout/LeftContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const LeftContainer = (props) => {
+    var greetingsText = "Grand Hotel Codecool"
+
+    return (<React.Fragment>
+        <div className="row">
+        <div className="col-6" >
+            <img className={"left-element"}
+                src={window.location.origin + "/pictures/cover3.png"}
+                style={imageStyle}
+            />
+            <div style={shading}></div>
+        </div>
+        <div className="col-6">
+        <h1 style={centerElement}><b>{greetingsText}</b></h1>
+        </div>
+        </div>
+    </React.Fragment>)
+}
+
+export default LeftContainer;
+
+const imageStyle = {
+    width: "100%",
+    height: "100%",
+}
+
+const shading = {
+    position: "absolute",
+    right: "-1px",
+    top: "0",
+    height: "100%",
+    width: "50%",
+    background: "linear-gradient(to right, rgba(255,255,255,0) 0%, #fff 97%, #fff 100%)",
+}
+
+const centerElement = {
+    zIndex: "1",
+    color: "black",
+    textShadow: "0 0 10px rgba(0,0,0,0.4)",
+    fontFamily: "Alegreya Sans SC, Alata",
+    fontSize: "1em",
+    textAlign: "left",
+}
