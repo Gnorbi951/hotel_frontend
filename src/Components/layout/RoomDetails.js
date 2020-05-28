e0a33cca3a0a0db065d8df40a7aca13c8c5253cf
Gnorbi951 <gnorbi951@gmail.com>
2020-05-28 11:21:17 +0200

    Fix buttons disappearing on zoom

--- src/Components/layout/RoomDetails.js
+++ src/Components/layout/RoomDetails.js
@@ -9,10 +9,6 @@ const RoomDetails = (props) => {
   return (
     <div style={{marginTop: "3em"}} >
         <RoomCategoryInfo currentRoom={currentRoom} />
-      {/*<h3>{currentRoom.name}</h3>*/}
-      {/*<h4>Description: {currentRoom.description}</h4>*/}
-      {/*<h4>{currentRoom.capacity}</h4>*/}
-      {/*<h4>{currentRoom.size} squaremeters</h4>*/}
       <form
         action={`http://localhost:8080/category/reserve/${currentRoom.id}`}
         method="POST"
