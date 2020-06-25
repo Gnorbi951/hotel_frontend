67ece6d0a761fb9663ea00ff05db6de0003b7233
kalnaipeter <kalnaipeter@gmail.com>
2020-06-25 13:38:29 +0200

    done

--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -2,41 +2,52 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ReservedRoomCard from "./layout/ReservedRoomCard";
 import ScrollAnimation from "react-animate-on-scroll";
+import {Link} from "react-router-dom";
+import styled from "styled-components";
 
 const Reservation = (props) => {
   const [rooms, setRooms] = useState(null);
-  const userRole = localStorage.getItem("roles");
+  const [userRoles, setUserRoles] = useState(null)
 
   useEffect(() => {
-    axios.get("http://localhost:8080/get-all-reservations").then((res) => {
+    axios.get("http://localhost:8080/get-my-reservations").then((res) => {
       setRooms(res.data);
+      setUserRoles(localStorage.getItem("roles"))
+
     });
   }, []);
 
   return (
       <div>
-      { userRole ? (
-          <p>asd</p>
-          ):(
-            <div>
-              <h1 className="text-in-middle gold-text-selection" style={mainTextStyle}>
-                Your Reservations
-              </h1>
-              <ScrollAnimation animateIn="fadeIn" delay={5} duration={1.5} offset={100}>
+        <h1 className="text-in-middle gold-text-selection" style={mainTextStyle}>
+          Your Reservations
+        </h1>
+        <ScrollAnimation animateIn="fadeIn" delay={5} duration={1.5} offset={100}>
+          {userRoles ? (
+              <div>
                 {rooms ? (
-                  <div className="card-deck ml-5 mr-3 mt-5">
-                    {rooms.map((room, index) => (
-                      <ReservedRoomCard room={room} key={index} />
-                    ))}
-                  </div>
+                    <div className="card-deck ml-5 mr-3 mt-5">
+                      {rooms.map((room, index) => (
+                          <ReservedRoomCard room={room} key={index}/>
+                      ))}
+                    </div>
                 ) : (
-                  <div className="card-deck ml-5 mr-3 mt-5">
-                    {/*<ReservedRoomCard room={null} />*/}
-                    Loading cards, please wait.
+                    <div className="card-deck ml-5 mr-3 mt-5">
+                      Loading cards, please wait.
+                    </div>)}
+              </div>
+          ) : (
+              <LogInStyle>
+                  <h3 className="text-in-middle gold-text-selection">
+                      You are not logged in.
+                      Please log in!
+                  </h3>
+                  <div>
+                <Link to={"/login"} className="btn btn-secondary card-text reserve-btn">Log In!</Link>
                   </div>
-                )}
-              </ScrollAnimation>
-            </div>)}
+              </LogInStyle>
+          )}
+        </ScrollAnimation>
       </div>
   );
 };
@@ -48,3 +59,12 @@ const mainTextStyle = {
   textShadow: "0 0 18px rgba(0,0,0,0.4)",
   fontFamily: "Alegreya Sans SC, Alata",
 };
+
+const LogInStyle = styled.div`
+    position: center;
+    margin-top: 5%;
+    & div {
+        margin-top: 1em;
+        margin-left: 48%;
+    }
+`
